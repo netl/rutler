089418175ff771e75514a35bc09b3165020dbe9d
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2024-09-30 19:44:05 +0300

    pt_camera now takes a pose which towards to turn to

--- rutler/joystick_rutler.py
+++ rutler/joystick_rutler.py
@@ -3,6 +3,7 @@ from tf_transformations import quaternion_from_euler
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PoseStamped, Quaternion
+import time
 
 class joystickDrive(Node):
     def __init__(self):
@@ -12,14 +13,19 @@ class joystickDrive(Node):
         self.sub = self.create_subscription(Joy, "joy", self.joystickCallback,10)
 
         #camera topic
-        self.camera = self.create_publisher(PoseStamped, "pt_camera", 10)
+        self.camera = self.create_publisher(PoseStamped, "/look_at", 10)
+
+        self.rateClock = time.time()
 
     def joystickCallback(self, msg):
+        t = time.time()
+        td = t-self.rateClock
+        self.rateClock = t
 
         #camera
         p = PoseStamped()
         f = Quaternion()
-        f.x, f.y, f.z, f.w = quaternion_from_euler(msg.axes[3],msg.axes[4],0)
+        f.x, f.y, f.z, f.w = quaternion_from_euler(td*msg.axes[3]*3.1415,td*msg.axes[4]*3.1415,0)
         p.pose.orientation = f
         self.camera.publish(p)
 
--- rutler/pt_gimbal.py
+++ rutler/pt_gimbal.py
@@ -1,5 +1,5 @@
-from tf_transformations import euler_from_quaternion
-from geometry_msgs.msg import PoseStamped
+from tf_transformations import euler_from_quaternion, quaternion_from_euler, quaternion_multiply
+from geometry_msgs.msg import PoseStamped, Pose
 from std_msgs.msg import Float32
 import rclpy
 from rclpy.node import Node
@@ -7,17 +7,51 @@ from rclpy.node import Node
 class panTilt(Node):
     def __init__(self):
         super().__init__('pan_tilt')
-        self.sub = self.create_subscription(PoseStamped, 'pt_camera', self.handlePose, 10)
+        self.sub = self.create_subscription(PoseStamped, 'look_at', self.handlePose, 10)
         self.panPublisher = self.create_publisher(Float32, 'rover/channel_3', 10)
         self.tiltPublisher = self.create_publisher(Float32, 'rover/channel_4', 10)
 
+        self.realPose = Pose()
+        self.posePublisher = self.create_publisher(PoseStamped, 'pt_camera', 10)
+        self.timer = self.create_timer(0.1, self.poseUpdater)
+        
+    def poseUpdater(self):
+        p = PoseStamped()
+        p.pose = self.realPose
+        self.posePublisher.publish(p)
+
     def handlePose(self, msg):
-        q = msg.pose.orientation
-        tilt, pan, roll = euler_from_quaternion([q.x,q.y,q.z,q.w])
+        
+        #turn quaternion in to array, because dunno
+        a = (
+            msg.pose.orientation.x,
+            msg.pose.orientation.y,
+            msg.pose.orientation.z,
+            msg.pose.orientation.w)
+        b = (
+            self.realPose.orientation.x,
+            self.realPose.orientation.y,
+            self.realPose.orientation.z,
+            self.realPose.orientation.w)
+
+        newTilt, newPan, newRoll = euler_from_quaternion(a)
+        tilt, pan, roll = euler_from_quaternion(b)
+       
+        #do the work
         t = Float32()
-        t.data = tilt
+        t.data = max(-3.1415/2, min(tilt+newTilt, 3.1415/2)) #clamp to servo limits
         p = Float32()
-        p.data = pan
+        p.data = max(-3.1415/2, min(pan+newPan, 3.1415/2)) #clamp to servo limits
+        result = quaternion_from_euler(t.data, p.data, 0) 
+
+        #wrangle shit back into place
+        self.realPose.orientation.x = result[0]
+        self.realPose.orientation.y = result[1]
+        self.realPose.orientation.z = result[2]
+        self.realPose.orientation.w = result[3]
+
+        t.data = t.data/(3.1415/2)
+        p.data = p.data/(3.1415/2)
         self.tiltPublisher.publish(t)
         self.panPublisher.publish(p)
 
