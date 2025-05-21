9d0cdca51c13e356a9033de270a81abef306b7d7
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2025-05-21 21:08:50 +0300

    trying to get the robot to move in rviz

--- ros-node/rutler/state_publisher.py
+++ ros-node/rutler/state_publisher.py
@@ -2,9 +2,10 @@ from math import sin, cos, pi
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Twist
 from sensor_msgs.msg import JointState
 from tf2_ros import TransformBroadcaster, TransformStamped
+from tf_transformations import quaternion_from_euler
 
 class StatePublisher(Node):
 
@@ -12,22 +13,24 @@ class StatePublisher(Node):
         rclpy.init()
         super().__init__('state_publisher')
 
+        #info for dead reckoning
+        self.twist_subscriber = self.create_subscription(Twist, '/cmd_vel', self.updateTwist, 10)
+
         qos_profile = QoSProfile(depth=10)
         self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
         self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
         self.nodeName = self.get_name()
         self.get_logger().info("{0} started".format(self.nodeName))
 
-        degree = pi / 180.0
         loop_rate = self.create_rate(30)
 
-        # robot state
-        tilt = 0.
-        tinc = degree
-        swivel = 0.
-        angle = 0.
-        height = 0.
-        hinc = 0.005
+        #variables
+        x = 0
+        y = 0
+        rotation = 0
+        self.velocity = 0
+        self.angularVelocity = 0
+        prevClock = self.get_clock().now().nanoseconds
 
         # message declarations
         odom_trans = TransformStamped()
@@ -43,30 +46,32 @@ class StatePublisher(Node):
                 now = self.get_clock().now()
                 joint_state.header.stamp = now.to_msg()
                 joint_state.name = ['swivel', 'tilt', 'periscope']
-                joint_state.position = [swivel, tilt, height]
+                joint_state.position = [rotation, 0, 1]
+
+                #time delta in nanoseconds
+                t = now.nanoseconds - prevClock
+                prevClock = now.nanoseconds
+
+                #dead reckoning
+                rotation += self.angularVelocity*self.velocity*t
+                x += self.velocity*sin(rotation)*t
+                y += self.velocity*cos(rotation)*t
+
 
                 # update transform
-                # (moving in a circle with radius=2)
                 odom_trans.header.stamp = now.to_msg()
-                odom_trans.transform.translation.x = cos(angle)*2
-                odom_trans.transform.translation.y = sin(angle)*2
-                odom_trans.transform.translation.z = 0.7
-                odom_trans.transform.rotation = \
-                    euler_to_quaternion(0, 0, angle + pi/2) # roll,pitch,yaw
+                odom_trans.transform.translation.x = x
+                odom_trans.transform.translation.y = y
+                odom_trans.transform.translation.z = 0.
+                q = quaternion_from_euler(0,0,float(rotation))
+                odom_trans.transform.rotation.x = q[0]
+                odom_trans.transform.rotation.y = q[1]
+                odom_trans.transform.rotation.z = q[2]
+                odom_trans.transform.rotation.w = q[3]
 
                 # send the joint state and transform
                 self.joint_pub.publish(joint_state)
                 self.broadcaster.sendTransform(odom_trans)
-
-                # Create new robot state
-                tilt += tinc
-                if tilt < -0.5 or tilt > 0.0:
-                    tinc *= -1
-                height += hinc
-                if height > 0.2 or height < 0.0:
-                    hinc *= -1
-                swivel += degree
-                angle += degree/4
 
                 # This will adjust as needed per iteration
                 loop_rate.sleep()
@@ -74,12 +79,10 @@ class StatePublisher(Node):
         except KeyboardInterrupt:
             pass
 
-def euler_to_quaternion(roll, pitch, yaw):
-    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
-    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
-    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
-    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
-    return Quaternion(x=qx, y=qy, z=qz, w=qw)
+    def updateTwist(self, msg):
+        self.velocity = msg.linear.x
+        self.angularVelocity = msg.angular.z
+        self.get_logger().info(f"{self.velocity},{self.angularVelocity}")
 
 def main():
     node = StatePublisher()
